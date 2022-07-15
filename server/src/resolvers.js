dd589479f42a1423570ca189dfe612f711103072
Aaron Jaeger <aaron.jaeger@mastery.net>
2022-07-15 13:49:17 -0500

    Finished resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,14 @@ const resolvers = {
     Query: {
         // returns an array of tracks that will be use to populate
         // the homepage grid of our web client
-        tracksForHome: (parents, args, context, info) => {}
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
 };
 
